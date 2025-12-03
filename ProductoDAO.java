78a5c4477e3279ae30dd18326dba40d917cd3e40
jerry51199 <al05056424@tecmilenio.mx>
2025-12-03 09:44:21 -0600

    Update database connection method in ProductoDAO
    Refactor database connection to use Conexion class.

--- ProductoDAO.java
+++ ProductoDAO.java
@@ -4,28 +4,14 @@ import com.papeleria.model.Producto;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
-import java.io.InputStream;
 
 public class ProductoDAO {
 
-    private Connection getConnection() throws Exception {
-        Properties prop = new Properties();
-        InputStream is = getClass().getClassLoader().getResourceAsStream("db.properties");
-        prop.load(is);
-        String url = prop.getProperty("db.url");
-        String user = prop.getProperty("db.user");
-        String pass = prop.getProperty("db.password");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(url, user, pass);
-    }
-
     public List<Producto> listarProductos() {
         List<Producto> lista = new ArrayList<>();
-        try (Connection con = getConnection();
+        try (Connection con = Conexion.getConnection();
              PreparedStatement ps = con.prepareStatement("SELECT * FROM productos");
              ResultSet rs = ps.executeQuery()) {
-
             while (rs.next()) {
                 lista.add(new Producto(
                         rs.getInt("id"),
