acaa147786d07a230590f4ec523cb1fc6875a764
jerry51199 <al05056424@tecmilenio.mx>
2025-12-03 09:36:56 -0600

    Implement LoginServlet for user authentication

---
+++ LoginServlet.java
@@ -0,0 +1,27 @@
+package com.papeleria.controller;
+
+import com.papeleria.dao.UsuarioDAO;
+import com.papeleria.model.Usuario;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+
+        UsuarioDAO dao = new UsuarioDAO();
+        Usuario u = dao.validarUsuario(usuario, password);
+
+        if (u != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("usuario", u);
+            response.sendRedirect("inventario.jsp");
+        } else {
+            response.sendRedirect("login.jsp?error=1");
+        }
+    }
+}
